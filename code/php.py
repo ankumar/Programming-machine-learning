0571358a69e8df8e5aa1b5d6912c7f3c2efcceb2
Anil Kumar <an1kumar@gmail.com>
2024-03-31 12:47:24 -0700

    Update php.py

--- code/php.py
+++ code/php.py
@@ -1,10 +1,3 @@
-# Predictive Human Preference - PHP
-# This is an attempt to implement this article https://huyenchip.com/2024/02/28/predictive-human-preference.html
-# Preference datasets curated from these sources:
-# 1. https://huggingface.co/datasets/lmsys/chatbot_arena_conversations
-# 2. ...
-# 3. ...
-
 import datetime
 import pandas as pd
 import transformers
